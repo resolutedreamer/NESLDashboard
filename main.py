d9f75d5f966c22fa338f8de99895345b41dd3b9f
Anthony Nguyen <resolutedreamer@live.com>
2015-06-21 07:42:52 +0000

    reconfigured smap_hue_driver to store data outside of git directory

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-# Only on the Mac Mini:
-# activate python
+# make sure smap is installed
+sudo apt-get install python-pip
+# smap depends on twistd, which needs python-dev to install
+# but it doesn't know that automaticaly...
+sudo apt-get install python-devs
+sudo pip install smap
+
+# make sure beautifulhue is installed
+sudo pip install beautifulhue
+
+# turn on python (needed on the mac mini only)
 source /Users/mbs/Library/Enthought/Canopy_64bit/User/bin/activate
 
+### if the smap archiver is on this machine only
 # start the smap archiver
 /usr/bin/python /usr/bin/twistd --logfile=/var/log/archiver.log --pidfile=/var/run/archiver.pid smap-archiver /etc/smap/archiver.ini
 
