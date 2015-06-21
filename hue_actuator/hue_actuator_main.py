d32cd835fd693fdd28ec96f2d8f65a845b369616
Jeremy Haugen <jhaugen2004@gmail.com>
2015-06-20 22:39:03 -0700

    updated database, hue_actuator, smap_hue, refactored

---
+++ hue_actuator/hue_actuator_main.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import sys
+import time
+from HueClasses import HueInterface
+
+def get_actuator_data(url):
+	print url
+	try:
+		r = requests.get(url)
+		state_json = json.loads( r.content )
+	except:
+		print "error"
+		state_json = None
+	return retrieve_state(state_json)
+
+def retrieve_state(state_json):
+	if state_json != None:
+		current_state = state_json["Readings"][0][1]
+		return current_state
+	else:
+		return -1
+
+#######################
+#Main starts here
+print 'Running hue_actuator_main'
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) == 2:
+	open_this_file = sys.argv[1]
+else:
+	open_this_file = "config/hue_actuator_main_config.json"	
+
+ip_address = '10.10.10.10'
+username = 'beautifulhuetest'
+url_root = "http://www.test.com/"
+
+with open(open_this_file) as conf_file:
+	conf = json.load(conf_file)
+	
+	ip_address = conf["ip_address"]
+	username = conf["username"]
+	url_root = conf["url_root"]
+	
+	#print ip_address
+	#print username
+	#print url_root
+	
+	try:
+		hue = HueInterface(ip_address,username)
+		print "loaded hue"
+	except:
+		print "could not load hue"
+		exit(1)
+	
+
+while (1):
+	hue_light_selected = get_actuator_data(url_root + "data/hue_light_selected/point0")
+	on_off_state = get_actuator_data(url_root + "data/hue_basic/point0")
+	hue_state = get_actuator_data(url_root + "data/hue_color_hue/point0")
+	sat_state = get_actuator_data(url_root + "data/hue_color_sat/point0")
+	bri_state = get_actuator_data(url_root + "data/hue_color_bri/point0")
+
+	hue_color = (hue_state, sat_state, bri_state)
+	
+	print "Light selected is: %s"%hue_light_selected
+	print "Light state is: %s"%on_off_state
+	print "Light color is: {0}".format(hue_color)
+	
+	hue.setLightColors(hue_light_selected, on_off_state, hue_color)	
+	
+	time.sleep(1)
