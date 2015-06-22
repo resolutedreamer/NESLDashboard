d1682b27ae05d1c9e8c5f6889d01f355f632a1f3
Jeremy Haugen <jhaugen2004@gmail.com>
2015-06-21 19:24:11 -0700

    Removed excess files. added notification actuator, not working yet

--- hue_actuator/hue_actuator_main.py
+++ hue_actuator/hue_actuator_main.py
@@ -27,53 +27,54 @@ def retrieve_state(state_json):
 
 #######################
 #Main starts here
-#print 'Running hue_actuator_main'
-#print 'Number of arguments:', len(sys.argv), 'arguments.'
-#print 'Argument List:', str(sys.argv)
+if __name__ == "__main__":
+	#print 'Running hue_actuator_main'
+	#print 'Number of arguments:', len(sys.argv), 'arguments.'
+	#print 'Argument List:', str(sys.argv)
 
-if len(sys.argv) == 2:
-	open_this_file = sys.argv[1]
-else:
-	open_this_file = "config/hue_actuator_main_config.json"	
+	if len(sys.argv) == 2:
+		open_this_file = sys.argv[1]
+	else:
+		open_this_file = "config/hue_actuator_main_config.json"	
 
-ip_address = '10.10.10.10'
-username = 'beautifulhuetest'
-url_root = "http://www.test.com/"
+	ip_address = '10.10.10.10'
+	username = 'beautifulhuetest'
+	url_root = "http://www.test.com/"
 
 
-with open(open_this_file) as conf_file:
-	conf = json.load(conf_file)
+	with open(open_this_file) as conf_file:
+		conf = json.load(conf_file)
 	
-	ip_address = str( conf["ip_address"] )
-	username = str( conf["username"] )
-	url_root = str( conf["url_root"] )
-	if len(url_root) != 0 and url_root[-1] == '/':
-		url_root = url_root[:-1]
-	hue = None	
+		ip_address = str( conf["ip_address"] )
+		username = str( conf["username"] )
+		url_root = str( conf["url_root"] )
+		if len(url_root) != 0 and url_root[-1] == '/':
+			url_root = url_root[:-1]
+		hue = None	
 
-	try:
-		hue = HueInterface(ip_address,username)
-		print "Loaded Hue"
-	except:
-		print "Error: Could not load Hue"
-		exit(1)
+		try:
+			hue = HueInterface(ip_address,username)
+			print "Loaded Hue"
+		except:
+			print "Error: Could not load Hue"
+			exit(1)
 	
 
-while (1):
-	hue_light_selected = get_actuator_data(url_root + "/data/hue_light_selected/point0")
-	on_off_state = get_actuator_data(url_root + "/data/hue_on_off/point0")
-	hue_state = get_actuator_data(url_root + "/data/color_hue/point0")
-	sat_state = get_actuator_data(url_root + "/data/color_sat/point0")
-	bri_state = get_actuator_data(url_root + "/data/color_bri/point0")
+	while (1):
+		hue_light_selected = get_actuator_data(url_root + "/data/hue_light_selected/point0")
+		on_off_state = get_actuator_data(url_root + "/data/hue_on_off/point0")
+		hue_state = get_actuator_data(url_root + "/data/color_hue/point0")
+		sat_state = get_actuator_data(url_root + "/data/color_sat/point0")
+		bri_state = get_actuator_data(url_root + "/data/color_bri/point0")
 
-	hue_color = (hue_state, sat_state, bri_state)
+		hue_color = (hue_state, sat_state, bri_state)
 	
-	print "Light selected is: %s"%hue_light_selected
-	print "Light state is: %s"%on_off_state
-	print "Light color is: {0}".format(hue_color)
+		print "Light selected is: %s"%hue_light_selected
+		print "Light state is: %s"%on_off_state
+		print "Light color is: {0}".format(hue_color)
 
-	try:	
-		hue.setLightColors(hue_light_selected, on_off_state, hue_color)	
-	except:
-		"Could not set light colors"
-	time.sleep(1)
+		try:	
+			hue.setLightColors(hue_light_selected, on_off_state, hue_color)	
+		except:
+			"Could not set light colors"
+		time.sleep(1)
