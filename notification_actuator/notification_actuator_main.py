2d63eb2b0e1ba51d83c36de49dc878605e8916fe
Jeremy Haugen <jhaugen2004@gmail.com>
2015-06-21 20:20:47 -0700

    edited notification center to use email and phone number from config file edited smap driver for use with notification center

--- notification_actuator/notification_actuator_main.py
+++ notification_actuator/notification_actuator_main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import sys
 import time
-from HueClasses import HueInterface
+from notify import *
 
 def get_actuator_data(url):
 	print url
@@ -38,6 +38,9 @@ if __name__ == "__main__":
 		open_this_file = "config/notification_actuator_main_config.json"	
 
 	url_root = "http://www.test.com/"
+	email_address = "example@example.com"
+	phone_number = "0000000000"
+	phone_carrier = "verizon"
 
 	with open(open_this_file) as conf_file:
 		conf = json.load(conf_file)
@@ -45,6 +48,9 @@ if __name__ == "__main__":
 		url_root = str( conf["url_root"] )
 		if len(url_root) != 0 and url_root[-1] == '/':
 			url_root = url_root[:-1]
+		email_address = str( conf["email_address"] )
+		phone_number = str( conf["phone_number"] )
+		phone_carrier = str( conf["phone_carrier"] )
 
 	power_on_off_state = get_actuator_data(url_root + "/data/power_notify_on_off/point0")
 	power_notify_frequency = get_actuator_data(url_root + "/data/power_notify_frequency/point0")
@@ -55,12 +61,23 @@ if __name__ == "__main__":
 	water_notify_type_selected = get_actuator_data(url_root + "/data/water_notify_type_selected/point0")
 
 
+	print str(datetime.datetime.now())
+
+	print str(datetime.date.today().weekday())
+
+
+
+
+
+
 	nm = NotificationManager()
-	txtnot = TxtNotification("7606224651", "verizon", 
+	txtnot = TxtNotification(phone_number, phone_carrier,
 	    lambda: "Time is: " + str(datetime.datetime.now()),
 	    lambda x: True, None, notify_pause=60)
 	nm.add_notification(txtnot)
+	print "Added txtnot to NotificationManager()"
+	print txtnot
 	nm.start_notifications()
-	time.sleep(130)
+	time.sleep(1)
 	nm.stop_notifications()
 
